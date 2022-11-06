499a8792f7ccb5225146c49b371fd4d56af797bf
mosmn <ithinkisee8@gmail.com>
2022-11-07 06:52:32 +0800

    created playround and game functions

--- main.js
+++ main.js
@@ -54,15 +54,104 @@ BEGIN
         RETURN computerSelection
     END FUNCTION
 
+    SET computerSelection = getComputerChoice()
 
+    FUNCTION playRound()
+        IF playerSelection === computerSelection
+            RETURN "It's a tie!"
+        ELSE IF playerSelection === 'rock' && computerSelection === 'scissors'
+            RETURN "You win! Rock beats Scissors"
+        ELSE IF playerSelection === 'rock' && computerSelection === 'paper'
+            RETURN "You lose! Paper beats Rock"
+        ELSE IF playerSelection === 'paper' && computerSelection === 'rock'
+            RETURN "You win! Paper beats Rock"
+        ELSE IF playerSelection === 'paper' && computerSelection === 'scissors'
+            RETURN "You lose! Scissors beats Paper"
+        ELSE IF playerSelection === 'scissors' && computerSelection === 'paper'
+            RETURN "You win! Scissors beats Paper"
+        ELSE IF playerSelection === 'scissors' && computerSelection === 'rock'
+            RETURN "You lose! Rock beats Scissors"
+        END IF
+    END FUNCTION
+
+    FUNCTION game()
+        SET playerScore = 0
+        SET computerScore = 0
+        FOR round = 1; round <= 5; round++
+            SET playerSelection = prompt("Rock, Paper, or Scissors?")
+            SET computerSelection = getComputerChoice()
+            IF playRound() === "You win! Rock beats Scissors" || playRound() === "You win! Paper beats Rock" || playRound() === "You win! Scissors beats Paper"
+                playerScore++
+            ELSE IF playRound() === "You lose! Paper beats Rock" || playRound() === "You lose! Scissors beats Paper" || playRound() === "You lose! Rock beats Scissors"
+                computerScore++
+            END IF
+        END FOR
+        IF playerScore > computerScore
+            SET winner = "You win!"
+        ELSE IF playerScore < computerScore
+            SET winner = "You lose!"
+        ELSE IF playerScore === computerScore
+            SET winner = "It's a tie!"
+        END IF
+            CONSOLE.LOG(playerScore)
+            CONSOLE.LOG(computerScore)
+        RETURN winner
+    END FUNCTION
+
+    CONSOLE.LOG(game())
 
 END
 */ 
-
 let getComputerChoice = () => {
     let computerSelection = ['rock', 'paper', 'scissors'];
     let randomIndex = Math.floor(Math.random() * computerSelection.length);
     computerSelection = computerSelection[randomIndex];
     return computerSelection;
 }
-console.log(getComputerChoice());
+
+let computerSelection = getComputerChoice();
+
+let playRound = () => {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return "You win! Rock beats Scissors";
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return "You lose! Paper beats Rock";
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return "You win! Paper beats Rock";
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return "You lose! Scissors beats Paper";
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return "You win! Scissors beats Paper";
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return "You lose! Rock beats Scissors";
+    }
+}
+
+let game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let round = 1; round <= 5; round++) {
+        playerSelection = prompt("Rock, Paper, or Scissors?");
+        computerSelection = getComputerChoice();
+        if (playRound() === "You win! Rock beats Scissors" || playRound() === "You win! Paper beats Rock" || playRound() === "You win! Scissors beats Paper") {
+            playerScore++;
+        } else if (playRound() === "You lose! Paper beats Rock" || playRound() === "You lose! Scissors beats Paper" || playRound() === "You lose! Rock beats Scissors") {
+            computerScore++;
+        }
+    }
+    if (playerScore > computerScore) {
+        winner = "You win!";
+    } else if (playerScore < computerScore) {
+        winner = "You lose!";
+    } else if (playerScore === computerScore) {
+        winner = "It's a tie!";
+    }
+    console.log(playerScore);
+    console.log(computerScore);
+    return winner;
+}
+
+console.log(game());
+
